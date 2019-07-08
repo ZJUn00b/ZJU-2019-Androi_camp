e728051369274f73605688afa30fb7024618687b
ZJUn00b <hsiahootch@gmail.com>
2019-07-09 00:20:56 +0800

    Camp Day 1 homework

---
+++ app/src/main/java/com/percy/helloworld/Main2Activity.java
@@ -0,0 +1,30 @@
+package com.percy.helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        ImageView imgView = (ImageView) this.findViewById(R.id.imageView3);
+        final int[] imageItems = new int[] { R.drawable.symbol };
+        imgView.setImageResource(imageItems[0]);
+        Button bt3 = (Button) findViewById(R.id.button4);
+        //创建监听器
+        bt3.setOnClickListener(listener);
+    }
+    Button.OnClickListener listener = new Button.OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+            startActivity(intent);
+            Main2Activity.this.finish();
+        }
+    };
+}
--- app/src/main/java/com/percy/helloworld/MainActivity.java
+++ app/src/main/java/com/percy/helloworld/MainActivity.java
@@ -1,37 +1,106 @@
 package com.percy.helloworld;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.BreakIterator;
 
-public class MainActivity extends AppCompatActivity  {
-    private Button button = null;
-    private Switch sw1 = null;
+public class MainActivity extends AppCompatActivity {
+    private SeekBar testSeekBar;
+    private TextView text2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //log检测
+        Log.d("mainactivity","init");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 获取界面组件
-        sw1 = (Switch) findViewById(R.id.switch1);
+        //根据ID值取得SeekBar对象和TextView对象
+        testSeekBar = (SeekBar)findViewById(R.id.seekBar);
+        text2 = (TextView)findViewById(R.id.textView2);
 
-        //
-        sw1.setOnClickListener(new CompoundButton.OnCheckedChangeListener(){
-    @Override
-    public void onCheckedchanged(CompoundButton compoundButton,boolean b){
-       if (compoundButton.isChecked()) {
-           Toast.makeText(MainActivity, this, "open", Toast.LENGTH_SHORT).show();
-       }else {
-           Toast.makeText(MainActivity, this, "close", Toast.LENGTH_SHORT).show();
-       }
-       }
+        testSeekBar.setMax(100); //设置进度条的最大值是100
+        testSeekBar.setProgress(30); //设置初始位置
+        //获取工具条
+        android.support.v7.widget.Toolbar toolbar1 = findViewById(R.id.toolbar);
+        toolbar1.setLogo(R.mipmap.ic_launcher);
+        //setSupportActionBar(toolbar1);
+        //按钮1,并为按钮1在Activity中定义一个内部类继承监听器接口（这里是OnClickListener）
+        Button bt1 = (Button) findViewById(R.id.button1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"you have clicked Button1",Toast.LENGTH_SHORT).show();
+            }
+        });
+        //按钮2
+        Button bt2 = (Button) findViewById(R.id.button3);
+        //为按钮2创建监听器
+        bt2.setOnClickListener(listener);
+        //switch组件
+        Switch  mswitch = (Switch)findViewById(R.id.switch1);
+        mswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                TextView textView1 = (TextView)findViewById(R.id.textView1);
+                if (isChecked)
+                {
+                    //打印调试信息
+                    Log.d("mainactivity","in  checkedchanged");
+                    textView1.setText("Mode: innormal");
 
-        })
+                }else {
+                    textView1.setText("Mode: normal");
+                }
+            }
+        });
+        //为seekbar写监听事件
+        testSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            //在进度开始改变时执行
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                int i= seekBar.getProgress(); //获取进度条的进度值
+                text2.setText("Start value is "+i); //在TextView上显示进度
+            }
+
+            //当进度发生改变时执行
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                if(fromUser) //判断是谁修改的进度值?
+                    text2.setText("Current value is "+progress+"");
+                else
+                    text2.setText("Current value is "+progress+"");
+            }
+
+            //在停止拖动时执行
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int i= seekBar.getProgress();  //获取进度条的进度值
+                text2.setText("Alpha Value "+i);
+            }
+        });
+
+
     }
+    Button.OnClickListener listener = new Button.OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(intent);
+            MainActivity.this.finish();
+        }
+    };
+
 }

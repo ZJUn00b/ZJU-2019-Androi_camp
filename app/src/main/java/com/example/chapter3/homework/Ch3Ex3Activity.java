9fc1a372e0ce6a4a0ec612c9e9dac42693b15bc4
ZJUn00b <hsiahootch@gmail.com>
2019-07-11 00:50:07 +0800

    complete mission 1 & 2 & 3

--- app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
+++ app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
@@ -1,7 +1,12 @@
 package com.example.chapter3.homework;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * 使用 ViewPager 和 Fragment 做一个简单版的好友列表界面
@@ -10,7 +15,8 @@ import android.os.Bundle;
  * 3. 对于好友列表 Fragment，使用 Lottie 实现 Loading 效果，在 5s 后展示实际的列表，要求这里的动效是淡入淡出
  */
 public class Ch3Ex3Activity extends AppCompatActivity {
-
+    private static final String TAG = "Ch3Ex3Activity";
+    private  String[] titles=new String[]{"好友列表","我的好友"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +25,32 @@ public class Ch3Ex3Activity extends AppCompatActivity {
 
 
         // TODO: ex3-1. 添加 ViewPager 和 Fragment 做可滑动界面
+        ViewPager viewPager=findViewById(R.id.viewpager);
+        TabLayout tabLayout=findViewById(R.id.tablayout);
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                Fragment fragment= new PlaceholderFragment();
+                Log.d(TAG, "getItem() called with: i = [" + i + "]");
+                return fragment;
+//                return new Fragment();
+            }
 
+            @Override
+            public int getCount() {
+                return titles.length;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position){
+                return titles[position];
+            }
+        });
 
 
         // TODO: ex3-2, 添加 TabLayout 支持 Tab
+        tabLayout.setupWithViewPager(viewPager,false);
     }
-}
+
+    }
+
--- app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
+++ app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
@@ -1,21 +1,36 @@
 package com.example.chapter3.homework;
 
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class PlaceholderFragment extends Fragment {
-
+    private View view;
+    private ListView listview;
+    private LottieAnimationView animationView;
+    private String[] data = {"小明","小红","小蓝","小绿","苏大强"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // TODO ex3-3: 修改 fragment_placeholder，添加 loading 控件和列表视图控件
-        return inflater.inflate(R.layout.fragment_placeholder, container, false);
+        view =  inflater.inflate(R.layout.fragment_placeholder, container, false);
+        listview = view.findViewById(R.id.lvItems);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,data);
+        listview.setAdapter(adapter);
+        listview.setVisibility(view.GONE);
+        animationView = view.findViewById(R.id.animation_view);
+        return view;
     }
 
     @Override
@@ -27,6 +42,19 @@ public class PlaceholderFragment extends Fragment {
             public void run() {
                 // 这里会在 5s 后执行
                 // TODO ex3-4：实现动画，将 lottie 控件淡出，列表数据淡入
+                ObjectAnimator animator1 = ObjectAnimator.ofFloat(animationView,
+                        "alpha",
+                        1f,
+                        0f);
+                animationView.setVisibility(view.GONE);
+                ObjectAnimator animator2 = ObjectAnimator.ofFloat(listview,
+                        "alpha",
+                        0f,
+                        1f);
+                listview.setVisibility(view.VISIBLE);
+                AnimatorSet animatorSet = new AnimatorSet();
+                animatorSet.playTogether(animator1,animator2);
+                animatorSet.start();
             }
         }, 5000);
     }
